f2fa778fa8220e3d0b388f21b46a6b565499fbb4
Dang Ljnh <dangthanhnhatlinh@gmail.com>
2023-06-07 17:46:46 +0700

    new: button toggle legend, tree structure

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,9 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1gB9-TLIr2YitYu72iplk0DNkUgQxvoSJhGTlgEtKiII/edit#gid=0';
+var googleDocURL =
+  "https://docs.google.com/spreadsheets/d/1g_GuhaMLy1nSYPzcd-ZEckpQgfl1X--iDu2KiHVIDSk/edit#gid=0";
+// var googleDocURL =
+//   "https://docs.google.com/spreadsheets/d/1gB9-TLIr2YitYu72iplk0DNkUgQxvoSJhGTlgEtKiII/edit#gid=0";
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
+// var googleApiKey = "AIzaSyAD3zFd8m2D-PQdAR5W_zFR0OZnPnEIXQo";
+var googleApiKey = "AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY";
